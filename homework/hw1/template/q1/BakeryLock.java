1d7a99a77d914bf892ab0f0d76f8e9a8b99b9cc3
Soniaroxana <sonia.marginean@gmail.com>
2015-06-16 21:36:10 -0500

    Working version of Bakery
    FastMutex still doesn't work

--- homework/hw1/template/q1/BakeryLock.java
+++ homework/hw1/template/q1/BakeryLock.java
@@ -1,43 +1,48 @@
 // TODO
 // Implement the bakery algorithm
 
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class BakeryLock implements MyLock {
-    int N;
-    volatile boolean[] choosing;
-    volatile int[] number;
+    private int N;
+    private AtomicBoolean[] choosing;
+    private AtomicInteger[] number;
 
     public BakeryLock(int numThread) {
         N = numThread;
-        choosing = new boolean[N];
-        number = new int[N];
+        choosing = new AtomicBoolean[N];
+        number = new AtomicInteger[N];
         for (int j=0; j<N; j++){
-            choosing[j] = false;
-            number[j] = 0;
+            choosing[j] = new AtomicBoolean();
+            choosing[j].set(false);
+            number[j] = new AtomicInteger();
+            number[j].set(0);
         }
 
     }
 
     @Override
     public void lock(int myId) {
-        choosing[myId]=true;
+        choosing[myId].set(true);
         for(int j=0; j<N; j++){
-            if (number[j]>number[myId]){
-                number[myId] = number[j];
+            if (number[j].get()>number[myId].get()){
+                number[myId].set(number[j].get());
             }
         }
-        number[myId] =number[myId]+1;
-        choosing[myId] = false;
+        number[myId].set(number[myId].get()+1);
+        choosing[myId].set(false);
 
         for (int j=0; j<N; j++) {
             if (j != myId) {
-                while (choosing[j]) ;
-                while ((number[j] != 0) && ((number[j] < number[myId]) || ((number[j] == number[myId]) && j < myId))) ;
+                while (choosing[j].get()) ;
+                while ((number[j].get() != 0) && ((number[j].get() < number[myId].get()) || ((number[j].get() == number[myId].get()) && j < myId))) ;
             }
         }
     }
 
     @Override
     public void unlock(int myId) {
-        number[myId] = 0;
+        number[myId].set(0);
     }
 }
