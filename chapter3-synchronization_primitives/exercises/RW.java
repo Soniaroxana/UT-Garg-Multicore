9ef3f3cd4553676fc6891148672ce070a2b280b0
vijaygarg1 <vkgarg@gmail.com>
2015-06-18 14:04:28 -0500

    exercises added

---
+++ chapter3-synchronization_primitives/exercises/RW.java
@@ -0,0 +1,11 @@
+class RW {
+    // TODO: declare your variables including semaphores
+    public void startRead() {
+    }
+    public void endRead() {
+    }
+    public void startWrite() {
+    }
+    public void endWrite() {
+    }
+}
