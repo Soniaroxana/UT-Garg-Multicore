8bd54b1d4ae5af9dd7007c7a2ada453ed2e5f402
fobofindia09 <neel.shah.528@gmail.com>
2015-08-01 12:01:02 -0500

    Fixed LockFreeQueue
    Removed early saved references
    Split head and tail AtomicMarkedReferences so enqueue actually works :)

--- homework/hw4/q1/QThread.java
+++ homework/hw4/q1/QThread.java
@@ -1,28 +1,30 @@
 /**
  * Created by soniamarginean on 7/25/15.
  */
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.*;
 import java.util.*;
 
-public class QThread extends Thread{
+public class QThread extends Thread {
     int myID;
     MyQueue<Integer> lockQ;
 
-    public QThread(int id, MyQueue<Integer> q){
+    public QThread(int id, MyQueue<Integer> q) {
         myID = id;
         lockQ = q;
     }
 
-    public void run(){
+    public void run() {
             System.out.println("Entering thread " + myID);
             if (myID%2==0) {
-                lockQ.deq();
+                System.out.println("Thread " + myID + " dequeues " + lockQ.deq());
             } else {
-                lockQ.enq(myID);
+                System.out.println("Thread " + myID + " enqueues " + myID);
+                        lockQ.enq(myID);
             }
             //lockQ.deq();
             //lockQ.enq(myID);
-
+            System.out.println("Leaving thread " + myID);
     }
 }
